0359fc8129e43b93a3ca754c446eaa13f313fccc
AllyHyeseongKim <ally3k@icloud.com>
2020-05-02 20:56:56 +0900

    Update the architecture declaring functions

--- assignment03/sort_project.c
+++ assignment03/sort_project.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+
+int *generate_input(int size_of_inputs);
+
+int *bubble_sort(int *input_data, int *running_time_of_sort);
+int *insertion_sort(int *input_data, int *running_time_of_sort);
+int *merge_sort(int *input_data, int *running_time_of_sort);
+int *quick_sort(int *input_data, int *running_time_of_sort);
+int *radix_sort(int *input_data, int *running_time_of_sort);
+int *bucket_sort(int *input_data, int *running_time_of_sort);
+
+void generate_table(int *running_time_of_sorts);
