cfd55b8b76ebe9fe8ac2f14ee009f93e42cbfde0
AllyHyeseongKim <ally3k@icloud.com>
2020-05-02 23:45:45 +0900

    Generate the input data

--- assignment03/sort_project.c
+++ assignment03/sort_project.c
@@ -4,7 +4,7 @@
 typedef struct dictionary{
 	char *key;
 	int value;
-}Dictionary;
+} Dictionary;
 
 int key_to_value(Dictionary *dictionary, char *key){
 	int value;
@@ -19,21 +19,57 @@ int key_to_value(Dictionary *dictionary, char *key){
 	return value;
 }
 
+void generate_input(int *input_data, int size_of_input);
 
-int *generate_input(int size_of_inputs);
+void bubble_sort(int *input_data, int *running_time_of_sort);
+void insertion_sort(int *input_data, int *running_time_of_sort);
+void merge_sort(int *input_data, int *running_time_of_sort);
+void quick_sort(int *input_data, int *running_time_of_sort);
+void radix_sort(int *input_data, int *running_time_of_sort);
+void bucket_sort(int *input_data, int *running_time_of_sort);
 
-int *bubble_sort(int *input_data, int *running_time_of_sort);
-int *insertion_sort(int *input_data, int *running_time_of_sort);
-int *merge_sort(int *input_data, int *running_time_of_sort);
-int *quick_sort(int *input_data, int *running_time_of_sort);
-int *radix_sort(int *input_data, int *running_time_of_sort);
-int *bucket_sort(int *input_data, int *running_time_of_sort);
-
-void generate_table(int *running_time_of_sorts);
-void print_table(int *running_time_of_sorts);
+void print_table(Dictionary *running_time_of_sorts);
 
 
 int main(void){
-	
+	Dictionary running_time_of_sorts[6];
+	running_time_of_sorts[0].key = "Bubble Sort";
+	running_time_of_sorts[1].key = "Insertion Sort";
+        running_time_of_sorts[2].key = "Merge Sort";
+        running_time_of_sorts[3].key = "Quick Sort";
+        running_time_of_sorts[4].key = "Radix Sort";
+        running_time_of_sorts[5].key = "Bucket Sort";
+
+	int input_10[10];
+	generate_input(input_10, 10);
+	int input_100[100];
+	generate_input(input_100, 100);
+	int input_1000[1000];
+	generate_input(input_1000, 1000);
+
 	return 0;
 }
+
+
+void generate_input(int *input_data, int size_of_input){
+	for(int i = 0; i < size_of_input; i++){
+		input_data[i] = size_of_input - i;
+	}
+}
+
+
+void bubble_sort(int *input_data, int *running_time_of_sort){
+}
+void insertion_sort(int *input_data, int *running_time_of_sort){
+}
+void merge_sort(int *input_data, int *running_time_of_sort){
+}
+void quick_sort(int *input_data, int *running_time_of_sort){
+}
+void radix_sort(int *input_data, int *running_time_of_sort){
+}
+void bucket_sort(int *input_data, int *running_time_of_sort){
+}
+
+void print_table(Dictionary *running_time_of_sorts){
+};
