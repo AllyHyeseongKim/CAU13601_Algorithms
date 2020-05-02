6b30924c3b082013fe32b91e13eb9646d1c4e80e
AllyHyeseongKim <ally3k@icloud.com>
2020-05-02 21:43:09 +0900

    Update structure of dictionary

--- assignment03/sort_project.c
+++ assignment03/sort_project.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+
+typedef struct dictionary{
+	char *key;
+	int value;
+}Dictionary;
+
+int key_to_value(Dictionary *dictionary, char *key){
+	int value;
+
+	for(int i = 0; i < sizeof(dictionary) / sizeof(dictionary[0]); i++){
+		if(dictionary[i].key == key){
+			value = dictionary[i].value;
+		}
+		else;
+	}
+
+	return value;
+}
+
+
 int *generate_input(int size_of_inputs);
 
 int *bubble_sort(int *input_data, int *running_time_of_sort);
@@ -10,3 +30,10 @@ int *radix_sort(int *input_data, int *running_time_of_sort);
 int *bucket_sort(int *input_data, int *running_time_of_sort);
 
 void generate_table(int *running_time_of_sorts);
+void print_table(int *running_time_of_sorts);
+
+
+int main(void){
+	
+	return 0;
+}
