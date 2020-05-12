452489900fb29fd5a37876e8a27cd91d6a5c8ce6
AllyHyeseongKim <ally3k@icloud.com>
2020-05-12 15:44:04 +0900

    Update malloc for memory allocation

--- assignment03/sort_project.c
+++ assignment03/sort_project.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 
-
-typedef struct dictionary{
-	char *key;
-	int value;
+typedef struct dictionary
+{
+        char *key;
+        int value;
 } Dictionary;
 
-int key_to_value(Dictionary *dictionary, char *key){
-	int value;
+int key_to_value(Dictionary *dictionary, char *key)
+{
+        int value;
 
-	for(int i = 0; i < sizeof(dictionary) / sizeof(dictionary[0]); i++){
-		if(dictionary[i].key == key){
-			value = dictionary[i].value;
-		}
-		else;
-	}
+        for (int i = 0; i < sizeof(dictionary) / sizeof(dictionary[0]); i++)
+        {
+                if (dictionary[i].key == key)
+                {
+                        value = dictionary[i].value;
+                }
+                else
+                        ;
+        }
 
-	return value;
+        return value;
 }
 
-typedef struct inputs{
-	int key;
-	int *data;
-	Dictionary running_time[6];
+typedef struct inputs
+{
+        int key;
+        int *data;
+        Dictionary running_time[6];
 } Inputs;
-
 
 void generate_input(int *input_data, int size_of_input);
 
@@ -37,18 +41,22 @@ void bucket_sort(int *input_data, int *running_time_of_sort);
 
 void print_table(Inputs *input_data);
 
+int main(void)
+{
+        Inputs input_data[3];
 
-int main(void){
-	Inputs input_data[3];
-	input_data[0].key = 10;
-	generate_input(input_data[0].data, input_data[0].key);
-	input_data[0].running_time[0].key = "Bubble Sort";
-	input_data[0].running_time[1].key = "Insertion Sort";
-	input_data[0].running_time[2].key = "Merge Sort";
-	input_data[0].running_time[3].key = "Quick Sort";
-	input_data[0].running_time[4].key = "Radix Sort";
-	input_data[0].running_time[5].key = "Bucket Sort";
-	input_data[1].key = 100;
+        input_data[0].key = 10;
+        input_data[0].data = malloc(sizeof(int) * input_data[0].key);
+        generate_input(input_data[0].data, input_data[0].key);
+        input_data[0].running_time[0].key = "Bubble Sort";
+        input_data[0].running_time[1].key = "Insertion Sort";
+        input_data[0].running_time[2].key = "Merge Sort";
+        input_data[0].running_time[3].key = "Quick Sort";
+        input_data[0].running_time[4].key = "Radix Sort";
+        input_data[0].running_time[5].key = "Bucket Sort";
+
+        input_data[1].key = 100;
+        input_data[1].data = malloc(sizeof(int) * input_data[1].key);
         generate_input(input_data[1].data, input_data[1].key);
         input_data[1].running_time[0].key = "Bubble Sort";
         input_data[1].running_time[1].key = "Insertion Sort";
@@ -56,7 +64,9 @@ int main(void){
         input_data[1].running_time[3].key = "Quick Sort";
         input_data[1].running_time[4].key = "Radix Sort";
         input_data[1].running_time[5].key = "Bucket Sort";
+
         input_data[2].key = 1000;
+        input_data[2].data = malloc(sizeof(int) * input_data[2].key);
         generate_input(input_data[2].data, input_data[2].key);
         input_data[2].running_time[0].key = "Bubble Sort";
         input_data[2].running_time[1].key = "Insertion Sort";
@@ -65,10 +75,10 @@ int main(void){
         input_data[2].running_time[4].key = "Radix Sort";
         input_data[2].running_time[5].key = "Bucket Sort";
 
-	bubble_sort(input_data[0].data, &input_data[0].running_time[0].value);
+        bubble_sort(input_data[0].data, &input_data[0].running_time[0].value);
         bubble_sort(input_data[1].data, &input_data[1].running_time[0].value);
         bubble_sort(input_data[2].data, &input_data[2].running_time[0].value);
-	insertion_sort(input_data[0].data, &input_data[0].running_time[1].value);
+        insertion_sort(input_data[0].data, &input_data[0].running_time[1].value);
         insertion_sort(input_data[1].data, &input_data[1].running_time[1].value);
         insertion_sort(input_data[2].data, &input_data[2].running_time[1].value);
         merge_sort(input_data[0].data, &input_data[0].running_time[2].value);
@@ -76,39 +86,48 @@ int main(void){
         merge_sort(input_data[2].data, &input_data[2].running_time[2].value);
         quick_sort(input_data[0].data, &input_data[0].running_time[3].value);
         quick_sort(input_data[1].data, &input_data[1].running_time[3].value);
-        quick_sort(input_data[2].data, &input_data[2].running_time[3].value);	
+        quick_sort(input_data[2].data, &input_data[2].running_time[3].value);
         radix_sort(input_data[0].data, &input_data[0].running_time[4].value);
         radix_sort(input_data[1].data, &input_data[1].running_time[4].value);
-        radix_sort(input_data[2].data, &input_data[2].running_time[4].value); 
+        radix_sort(input_data[2].data, &input_data[2].running_time[4].value);
         bucket_sort(input_data[0].data, &input_data[0].running_time[5].value);
         bucket_sort(input_data[1].data, &input_data[1].running_time[5].value);
-        bucket_sort(input_data[2].data, &input_data[2].running_time[5].value); 
+        bucket_sort(input_data[2].data, &input_data[2].running_time[5].value);
 
-	print_table(input_data);
+        print_table(input_data);
 
-	return 0;
-}
+        free(input_data[0].data);
+        free(input_data[1].data);
+        free(input_data[2].data);
 
-
-void generate_input(int *input_data, int size_of_input){
-	for(int i = 0; i < size_of_input; i++){
-		input_data[i] = size_of_input - i;
-	}
+        return 0;
 }
 
-
-void bubble_sort(int *input_data, int *running_time_of_sort){
-}
-void insertion_sort(int *input_data, int *running_time_of_sort){
-}
-void merge_sort(int *input_data, int *running_time_of_sort){
-}
-void quick_sort(int *input_data, int *running_time_of_sort){
-}
-void radix_sort(int *input_data, int *running_time_of_sort){
-}
-void bucket_sort(int *input_data, int *running_time_of_sort){
+void generate_input(int *input_data, int size_of_input)
+{
+        for (int i = 0; i < size_of_input; i++)
+        {
+                input_data[i] = size_of_input - i;
+        }
 }
 
-void print_table(Inputs *input_data){
-};
+void bubble_sort(int *input_data, int *running_time_of_sort)
+{
+}
+void insertion_sort(int *input_data, int *running_time_of_sort)
+{
+}
+void merge_sort(int *input_data, int *running_time_of_sort)
+{
+}
+void quick_sort(int *input_data, int *running_time_of_sort)
+{
+}
+void radix_sort(int *input_data, int *running_time_of_sort)
+{
+}
+void bucket_sort(int *input_data, int *running_time_of_sort)
+{
+}
+
+void print_table(Inputs *input_data){};
